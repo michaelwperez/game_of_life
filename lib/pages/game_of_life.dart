60c4a112a73aa4071540101f7934eb68adf7510e
michaelwperez <michaelwperez@gmail.com>
2024-01-16 21:45:54 -0800

    Format document

--- lib/pages/game_of_life.dart
+++ lib/pages/game_of_life.dart
@@ -21,11 +21,11 @@ class GameOfLifePage extends StatefulWidget {
 class _GameOfLifePageState extends State<GameOfLifePage> {
   var fabText = 'Start';
   var isPlaying = false;
-  int width = 30; 
+  int width = 30;
   int height = 30;
   List<List<GridItem>> gridItems = [];
 
-    @override
+  @override
   void initState() {
     super.initState();
     initializeGrid();
@@ -43,11 +43,10 @@ class _GameOfLifePageState extends State<GameOfLifePage> {
 
   void onPressed() {
     setState(() {
-    fabText = fabText == 'Start' ? 'Pause' : 'Start';
-    isPlaying = !isPlaying;
-    gameLoop();
-    }
-    );
+      fabText = fabText == 'Start' ? 'Pause' : 'Start';
+      isPlaying = !isPlaying;
+      gameLoop();
+    });
   }
 
   void gameLoop() {
@@ -55,20 +54,24 @@ class _GameOfLifePageState extends State<GameOfLifePage> {
       return;
     }
 
-     List<List<GridItem>> newGrid = List.generate(
-    height,
-    (i) => List.generate(
-      width,
-      (j) => GridItem(x: i, y: j, living: gridItems[i][j].living, neighbors: gridItems[i][j].neighbors),
-    ),
-  );
+    List<List<GridItem>> newGrid = List.generate(
+      height,
+      (i) => List.generate(
+        width,
+        (j) => GridItem(
+            x: i,
+            y: j,
+            living: gridItems[i][j].living,
+            neighbors: gridItems[i][j].neighbors),
+      ),
+    );
     //play
-    
-    for(int i = 0; i < height; i++) {
-      for(int j = 0; j < width; j++) {
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
         bool hasChanged = false;
         GridItem cell = gridItems[i][j];
-            //if living cell  has 0-1 or 4+ neighbors, kill it
+        //if living cell  has 0-1 or 4+ neighbors, kill it
         if (cell.living && (cell.neighbors <= 1 || cell.neighbors >= 4)) {
           newGrid[i][j].living = false;
           hasChanged = true;
@@ -78,7 +81,7 @@ class _GameOfLifePageState extends State<GameOfLifePage> {
           hasChanged = true;
         }
 
-        if(hasChanged) {
+        if (hasChanged) {
           incrementDecrementNeighbors(newGrid, i, j, newGrid[i][j].living);
         }
       }
@@ -86,50 +89,54 @@ class _GameOfLifePageState extends State<GameOfLifePage> {
     setState(() {
       gridItems = newGrid;
     });
-    
+
     Future.delayed(Duration(milliseconds: 500), () {
       gameLoop();
     });
-
   }
 
   incrementDecrementNeighbors(grid, row, col, isLiving) {
     for (int i = -1; i < 2; i++) {
-        for (int j = -1; j < 2; j++) {
-          // Skip the center cell itself
-          if (i == 0 && j == 0) {
-            continue;
-          }
+      for (int j = -1; j < 2; j++) {
+        // Skip the center cell itself
+        if (i == 0 && j == 0) {
+          continue;
+        }
 
-          int newRow = row + i;
-          int newCol = col + j;
+        int newRow = row + i;
+        int newCol = col + j;
 
-          // Check bounds
-          if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length) {
-            if (isLiving) {
-              grid[newRow][newCol].neighbors++;
-            } else {
-              grid[newRow][newCol].neighbors--;
-            }
+        // Check bounds
+        if (newRow >= 0 &&
+            newRow < grid.length &&
+            newCol >= 0 &&
+            newCol < grid[0].length) {
+          if (isLiving) {
+            grid[newRow][newCol].neighbors++;
+          } else {
+            grid[newRow][newCol].neighbors--;
           }
         }
       }
     }
+  }
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: 
-        Center(
-          child: SquaresGrid(isPlaying: isPlaying, gridItems: gridItems, width: width, height: height)
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-               onPressed: onPressed,
-               icon: Icon(Icons.play_arrow), 
-               label: Text(fabText),
-        ),
+      body: Center(
+          child: SquaresGrid(
+              isPlaying: isPlaying,
+              gridItems: gridItems,
+              width: width,
+              height: height)),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: onPressed,
+        icon: Icon(Icons.play_arrow),
+        label: Text(fabText),
+      ),
     );
   }
 }
@@ -139,25 +146,28 @@ class SquaresGrid extends StatefulWidget {
   int width;
   int height;
   List<List<GridItem>> gridItems;
-  SquaresGrid({required this.isPlaying, required this.gridItems, required this.width, required this.height});
+  SquaresGrid(
+      {required this.isPlaying,
+      required this.gridItems,
+      required this.width,
+      required this.height});
   @override
   State<SquaresGrid> createState() => _SquaresGridState();
 }
 
 class _SquaresGridState extends State<SquaresGrid> {
   @override
-
   Widget build(BuildContext context) {
     return GridView.builder(
       itemCount: widget.width * widget.height,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: widget.width, 
+        crossAxisCount: widget.width,
       ),
       itemBuilder: (BuildContext context, int index) {
         int row = index ~/ widget.width;
         int col = index % widget.height;
 
- return GestureDetector(
+        return GestureDetector(
           onTap: () {
             setState(() {
               handleCellTap(row, col);
@@ -184,13 +194,20 @@ class _SquaresGridState extends State<SquaresGrid> {
     bool isLiving = widget.gridItems[row][col].living;
     for (int i = -1; i < 2; i++) {
       for (int j = -1; j < 2; j++) {
-        if ( row + i < 0 || col + j < 0 || row + i >= widget.width || col + j >= widget.height || (i == 0 && j == 0)) {
+        if (row + i < 0 ||
+            col + j < 0 ||
+            row + i >= widget.width ||
+            col + j >= widget.height ||
+            (i == 0 && j == 0)) {
           continue;
         }
         if (isLiving) {
           widget.gridItems[row + i][col + j].neighbors++;
-        }
-        else if (!isLiving && row + i >= 0 && col + j >= 0 && row + i < widget.width && col + j < widget.height) {
+        } else if (!isLiving &&
+            row + i >= 0 &&
+            col + j >= 0 &&
+            row + i < widget.width &&
+            col + j < widget.height) {
           widget.gridItems[row + i][col + j].neighbors--;
         }
       }
@@ -199,33 +216,36 @@ class _SquaresGridState extends State<SquaresGrid> {
 
   String getCellText(int row, int col) {
     bool isDebug = false;
-    if (widget.gridItems.isEmpty)
-    return '';
+    if (widget.gridItems.isEmpty) return '';
     if (isDebug) {
       return '${widget.gridItems[row][col].neighbors}';
     }
-      return '';
+    return '';
   }
 }
 
-class GridItem extends StatefulWidget{
+class GridItem extends StatefulWidget {
   int x;
   int y;
   bool living = false;
   int neighbors = 0;
 
-  GridItem({required this.x, required this.y, required this.living, required this.neighbors});
-  
-@override
+  GridItem(
+      {required this.x,
+      required this.y,
+      required this.living,
+      required this.neighbors});
+
+  @override
   _GridItemState createState() => _GridItemState();
-  }
+}
 
 class _GridItemState extends State<GridItem> {
   Color _containerColor = Colors.grey;
 
   void _changeColor() {
     setState(() {
-      _containerColor = widget.living ? MyColors.Teal :MyColors.DarkTeal;
+      _containerColor = widget.living ? MyColors.Teal : MyColors.DarkTeal;
     });
   }
 
